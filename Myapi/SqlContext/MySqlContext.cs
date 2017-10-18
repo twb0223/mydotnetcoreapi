e0b6eedd5abcff0b5b9cadc48b521d7fa9dbdb89
twb0223 <twb0223@gmail.com>
2017-10-18 13:35:25 +0800

    change database connection

--- Myapi/SqlContext/MySqlContext.cs
+++ Myapi/SqlContext/MySqlContext.cs
@@ -11,13 +11,7 @@ namespace Myapi.SqlContext
     {
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Application> Applications { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-<<<<<<< HEAD
-                   => optionsBuilder.UseMySql(@"Server=bdm275410299.my3w.com;database=bdm275410299_db;uid=bdm275410299;pwd=tanwenbin");
-
-=======
-                   => optionsBuilder.UseMySql(@"Server=localhost;database=ef;uid=root;pwd=root");
->>>>>>> c40a4f250b90192748abb914408bd62b5dcc0d61
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySql(@"Server=bdm275410299.my3w.com;database=bdm275410299_db;uid=bdm275410299;pwd=tanwenbin");
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
