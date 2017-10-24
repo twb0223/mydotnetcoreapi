e52433e5ba00209b2c4686c1e076cdc43c250691
twb0223 <twb0223@gmail.com>
2017-10-24 16:16:26 +0800

    add http get token

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,9 @@
 ﻿using IdentityModel.Client;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Client
@@ -11,40 +13,68 @@ namespace Client
         static void Main(string[] args)
         {
             MainAsync().GetAwaiter().GetResult();
+
         }
         private static async Task MainAsync()
         {
             Console.Title = "Credentials Client";
-            // 从元数据中发现客户端
-            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            
+            //// 1.从元数据中发现客户端
+            //var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            //// 请求令牌
+            //var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            //var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
 
-            // 请求令牌
-            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
-            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+            //if (tokenResponse.IsError)
+            //{
+            //    Console.WriteLine(tokenResponse.Error);
+            //    return;
+            //}
 
-            if (tokenResponse.IsError)
+            //Console.WriteLine(tokenResponse.Json);
+            //Console.WriteLine("\n\n");
+
+            //2.直接通过Http 访问得到Token http://localhost:5000/connect/token,
+            //需要构造请求头相关信息。
+            List<KeyValuePair<string, string>> formdata = new List<KeyValuePair<string, string>>
             {
-                Console.WriteLine(tokenResponse.Error);
-                return;
-            }
+                new KeyValuePair<string, string>("grant_type","client_credentials"),
+                new KeyValuePair<string, string>("client_id","client"),
+                new KeyValuePair<string, string>("client_secret","secret")
+            };
+            var token =await HttpPostAsync("http://localhost:5000", "connect/token", formdata);
+            
+            Console.WriteLine(token);
 
-            Console.WriteLine(tokenResponse.Json);
-            Console.WriteLine("\n\n");
-
-            // 调用api
+            //// 调用api
+            //var client = new HttpClient();
+            //client.SetBearerToken(tokenResponse.AccessToken);
+            //var response = await client.GetAsync("http://localhost:5001/identity");
+            //if (!response.IsSuccessStatusCode)
+            //{
+            //    Console.WriteLine(response.StatusCode);
+            //}
+            //else
+            //{
+            //    var content = await response.Content.ReadAsStringAsync();
+            //    Console.WriteLine(JArray.Parse(content));
+            //}
+        }
+        public async static Task<string> HttpPostAsync(string uri, string url, List<KeyValuePair<string, string>> formData = null, string charset = "UTF-8", string mediaType = "application/x-www-form-urlencoded")
+        {
+            string tokenUri = url;
             var client = new HttpClient();
-            client.SetBearerToken(tokenResponse.AccessToken);
-
-            var response = await client.GetAsync("http://localhost:5001/identity");
-            if (!response.IsSuccessStatusCode)
+            client.BaseAddress = new Uri(uri);
+            HttpContent content = new FormUrlEncodedContent(formData);
+            content.Headers.ContentType = new MediaTypeHeaderValue(mediaType);
+            content.Headers.ContentType.CharSet = charset;
+            for (int i = 0; i < formData.Count; i++)
             {
-                Console.WriteLine(response.StatusCode);
+                content.Headers.Add(formData[i].Key, formData[i].Value);
             }
-            else
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(JArray.Parse(content));
-            }
+            var resp = await client.PostAsync(tokenUri, content);
+            var token = await resp.Content.ReadAsStringAsync();
+            return token;
         }
     }
 }
