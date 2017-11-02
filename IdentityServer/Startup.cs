f6752f92a5dcdc32adaf1b631dd2688147106c81
twb0223 <twb0223@gmail.com>
2017-11-02 14:18:53 +0800

    add

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using IdentityServer.Service;
+using IdentityServer4.Services;
+using IdentityServer4.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using IdentityServer4.Validation;
-using IdentityServer.Service;
-using IdentityServer4.Services;
-using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace IdentityServer
@@ -62,36 +57,36 @@ namespace IdentityServer
 
         private void InitializeDatabase(IApplicationBuilder app)
         {
-            // using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-            // {
-            //     serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
-            //     var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-            //     context.Database.Migrate();
-            //     if (!context.Clients.Any())
-            //     {
-            //         foreach (var client in Config.GetClients())
-            //         {
-            //             context.Clients.Add(client.ToEntity());
-            //         }
-            //         context.SaveChanges();
-            //     }
-            //     if (!context.IdentityResources.Any())
-            //     {
-            //         foreach (var resource in Config.GetIdentityResources())
-            //         {
-            //             context.IdentityResources.Add(resource.ToEntity());
-            //         }
-            //         context.SaveChanges();
-            //     }
-            //     if (!context.ApiResources.Any())
-            //     {
-            //         foreach (var resource in Config.GetApiResources())
-            //         {
-            //             context.ApiResources.Add(resource.ToEntity());
-            //         }
-            //         context.SaveChanges();
-            //     }
-            // }
+            //using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            //{
+            //    serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+            //    var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            //    context.Database.Migrate();
+            //    if (!context.Clients.Any())
+            //    {
+            //        foreach (var client in Config.GetClients())
+            //        {
+            //            context.Clients.Add(client.ToEntity());
+            //        }
+            //        context.SaveChanges();
+            //    }
+            //    if (!context.IdentityResources.Any())
+            //    {
+            //        foreach (var resource in Config.GetIdentityResources())
+            //        {
+            //            context.IdentityResources.Add(resource.ToEntity());
+            //        }
+            //        context.SaveChanges();
+            //    }
+            //    if (!context.ApiResources.Any())
+            //    {
+            //        foreach (var resource in Config.GetApiResources())
+            //        {
+            //            context.ApiResources.Add(resource.ToEntity());
+            //        }
+            //        context.SaveChanges();
+            //    }
+            //}
         }
 
     }
